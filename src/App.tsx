b96b676818ee49eafd254994779e19ba313760b0
Renato Machado <renatomachado176@gmail.com>
2024-11-24 15:20:42 +0000

    - summary page with data from server - price and data formatter

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 
 import { Transactions } from "./pages/Transactions";
+import { TransactionsProvider } from "./contexts/TransactionsContexts";
 
 export function App(){
   return(
     <ThemeProvider theme={defaultTheme}>
-      <Transactions />
-
       <GlobalStyle />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from "react"
+import { useContext } from "react"
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
 import { SearchForm } from "./components/SearchForm"
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles"
-
-interface Transaction {
-    id: number,
-    description: string,
-    type: "income" | "outcome",
-    price: number,
-    category: string,
-    createdAt: string
-}
+import { TransactionsContext } from "../../contexts/TransactionsContexts"
+import { dateFormatter, priceFormatter } from "../../utils/formatter"
 
 export function Transactions(){
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransactions(){
-        const response = await fetch("http://localhost:3000/transactions")
-        const data = await response.json()
-        setTransactions(data)
-    }
-
-    useEffect(() => {
-        loadTransactions()
-    }, [])
+    const { transactions } = useContext(TransactionsContext)
 
     return(
         <div>
@@ -40,11 +23,12 @@ export function Transactions(){
                                     <td width="50%">{transaction.description}</td>
                                     <td>
                                         <PriceHighLight variant={transaction.type}>
-                                            {transaction.price}
+                                            {transaction.type === "outcome" && "- "}
+                                            {priceFormatter.format(transaction.price)}
                                         </PriceHighLight>
                                     </td>
                                     <td>{transaction.category}</td>
-                                    <td>{transaction.createdAt}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                                 </tr>
                             )
                         })}
